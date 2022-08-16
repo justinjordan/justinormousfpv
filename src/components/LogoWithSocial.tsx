8683292fe1b60370cc8146c3e5f4a0a7d8f53af6
Justin Jordan <justin@justinjordan.io>
2022-08-16 03:58:19 -0500

    More style tweaks

--- src/components/LogoWithSocial.tsx
+++ src/components/LogoWithSocial.tsx
@@ -4,22 +4,23 @@ import { FaTiktok } from "react-icons/fa";
 export const LogoWithSocial: React.FC = () => {
   return (
     <div className="max-w-full max-h-full">
-      <img
-        className="pointer-events-none select-none opacity-80 m-h-full"
-        style={{ width: "50rem", height: "auto" }}
-        src="/logo-with-text.svg"
-        alt="logo"
-      />
-      <div className="flex gap-8 justify-center h-14">
-        <a href="https://youtube.com/justinormousfpv">
-          <AiFillYoutube size="100%" />
-        </a>
-        <a href="https://www.instagram.com/justinormousfpv">
-          <AiFillInstagram size="100%" />
-        </a>
-        <a href="https://www.tiktok.com/@justinormousfpv">
-          <FaTiktok size="100%" />
-        </a>
+      <div>
+        <img
+          className="pointer-events-none select-none m-h-full w-60 lg:w-[50rem] h-auto"
+          src="/logo-with-text.svg"
+          alt="logo"
+        />
+        <div className="flex gap-8 justify-center h-14">
+          <a href="https://youtube.com/justinormousfpv">
+            <AiFillYoutube size="100%" />
+          </a>
+          <a href="https://www.instagram.com/justinormousfpv">
+            <AiFillInstagram size="100%" />
+          </a>
+          <a href="https://www.tiktok.com/@justinormousfpv">
+            <FaTiktok size="100%" />
+          </a>
+        </div>
       </div>
     </div>
   );
