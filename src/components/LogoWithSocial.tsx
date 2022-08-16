13cd5024ab3b67498713ee44da826a258263edf4
Justin Jordan <justin@justinjordan.io>
2022-08-16 04:07:25 -0500

    Fit logo

--- src/components/LogoWithSocial.tsx
+++ src/components/LogoWithSocial.tsx
@@ -3,24 +3,22 @@ import { FaTiktok } from "react-icons/fa";
 
 export const LogoWithSocial: React.FC = () => {
   return (
-    <div className="max-w-full max-h-full">
-      <div>
-        <img
-          className="pointer-events-none select-none m-h-full w-60 md:w-[20rem] lg:w-[30rem] lg:w-[40rem] xl:w-[50rem] h-auto"
-          src="/logo-with-text.svg"
-          alt="logo"
-        />
-        <div className="flex gap-8 justify-center h-14">
-          <a href="https://youtube.com/justinormousfpv">
-            <AiFillYoutube size="100%" />
-          </a>
-          <a href="https://www.instagram.com/justinormousfpv">
-            <AiFillInstagram size="100%" />
-          </a>
-          <a href="https://www.tiktok.com/@justinormousfpv">
-            <FaTiktok size="100%" />
-          </a>
-        </div>
+    <div className="max-w-full max-h-full w-fit">
+      <img
+        className="pointer-events-none select-none m-h-full w-60 md:w-[20rem] lg:w-[30rem] lg:w-[40rem] xl:w-[50rem] h-auto"
+        src="/logo-with-text.svg"
+        alt="logo"
+      />
+      <div className="flex gap-8 justify-center h-14">
+        <a href="https://youtube.com/justinormousfpv">
+          <AiFillYoutube size="100%" />
+        </a>
+        <a href="https://www.instagram.com/justinormousfpv">
+          <AiFillInstagram size="100%" />
+        </a>
+        <a href="https://www.tiktok.com/@justinormousfpv">
+          <FaTiktok size="100%" />
+        </a>
       </div>
     </div>
   );
