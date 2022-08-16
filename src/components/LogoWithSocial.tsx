bd34346ff2618caa34a96d822c518a040fd5a1f3
Justin Jordan <justin.jordan@commentsold.com>
2022-08-16 18:49:17 -0500

    Center logo

--- src/components/LogoWithSocial.tsx
+++ src/components/LogoWithSocial.tsx
@@ -4,11 +4,13 @@ import { FaTiktok } from "react-icons/fa";
 export const LogoWithSocial: React.FC = () => {
   return (
     <div className="flex flex-col max-w-full max-h-full w-fit">
-      <img
-        className="pointer-events-none select-none m-h-full w-60 md:w-[20rem] lg:w-[30rem] lg:w-[40rem] xl:w-[50rem] h-auto"
-        src="/logo-with-text.svg"
-        alt="logo"
-      />
+      <div className="flex justify-center">
+        <img
+          className="pointer-events-none select-none m-h-full w-60 md:w-[20rem] lg:w-[30rem] lg:w-[40rem] xl:w-[50rem] h-auto"
+          src="/logo-with-text.svg"
+          alt="logo"
+        />
+      </div>
       <div className="flex gap-8 justify-center h-14">
         <a href="https://youtube.com/justinormousfpv">
           <AiFillYoutube size="100%" />
